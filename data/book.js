924f51078eef69f972e48780eba9b6f1f68fee21
Seán de Búrca <leftmostcat@gmail.com>
2014-05-17 14:54:54 -0600

    Rewrite Book insert with promises

--- data/book.js
+++ data/book.js
@@ -1,4 +1,5 @@
 var async = require('async');
+var q = require('q');
 var util = require('util');
 
 var super_ = require('./coreentity');
@@ -18,63 +19,82 @@ util.inherits(Book, super_);
 
 Book.prototype.insert = function(data, callback) {
 	this.sql.begin(function(err, cursor) {
-		cursor.query('INSERT INTO revision(editor_id) VALUES($1) RETURNING revision_id', [ data.editor_id ], function(err, results) {
-			var revision_id = results[0].revision_id;
+		if (err)
+			return callback(err);
 
-			cursor.query('INSERT INTO book(master_revision_id) VALUES($1) RETURNING book_id', [ revision_id ], function(err, results) {
-				var book_id = results[0].book_id;
+		var revision_id, book_id;
 
-				cursor.query('INSERT INTO creator_credit(pre_phrase) VALUES($1) RETURNING creator_credit_id', [ data.pre_phrase ], function(err, results) {
-					var creator_credit_id = results[0].creator_credit_id;
+		cursor.query('INSERT INTO revision(editor_id) VALUES($1) RETURNING revision_id', [ data.editor_id ]).then(function(results) {
+			revision_id = results[0].revision_id;
 
-					async.parallel({
-						credits: function(parallelback) {
-							async.each(data.credits, function(name, eachback) {
-								cursor.query('INSERT INTO creator_credit_name(creator_credit_id, position, creator_id, name, join_phrase)' +
-									' VALUES($1, $2, $3, $4, $5)',
-									[ creator_credit_id, name.position, name.id, name.name, name.join_phrase ], function(err) {
-										eachback(err);
-									});
+			return cursor.query('INSERT INTO book(master_revision_id) VALUES($1) RETURNING book_id', [ revision_id ]);
+		}).then(function(results) {
+			book_id = results[0].book_id;
+
+			return cursor.query('INSERT INTO creator_credit(pre_phrase) VALUES($1) RETURNING creator_credit_id', [ data.pre_phrase ]);
+		}).then(function(results) {
+			var deferred = q.defer();
+			var creator_credit_id = results[0].creator_credit_id;
+
+			async.parallel([
+				function(callback) {
+					async.each(data.credits, function(name, callback) {
+						cursor.query('INSERT INTO creator_credit_name(creator_credit_id, position, creator_id, name, join_phrase)' +
+							' VALUES($1, $2, $3, $4, $5)',
+							[ creator_credit_id, name.position, name.id, name.name, name.join_phrase ]).done(
+							function() {
+								callback();
 							},
 							function(err) {
-								parallelback(err);
-							});
-						},
-						book: function(parallelback) {
-							cursor.query('INSERT INTO book_data(name, creator_credit_id, book_type_id, comment)' + 
-								' VALUES($1, $2, $3, $4) RETURNING book_data_id',
-								[ data.title, creator_credit_id, data.book_primary_type_id, data.comment ], function(err, results) {
-									if (err)
-										return parallelback(err);
-
-									var book_data_id = results[0].book_data_id;
-
-									cursor.query('INSERT INTO book_tree(book_data_id)' +
-										' VALUES($1) RETURNING book_tree_id', [ book_data_id ], function(err, results) {
-											if (err)
-												return parallelback(err);
-
-											var book_tree_id = results[0].book_tree_id;
-
-											cursor.query('INSERT INTO book_revision(revision_id, book_id, book_tree_id)' +
-												' VALUES($1, $2, $3)', [ revision_id, book_id, book_tree_id ], function(err) {
-													parallelback(err, book_id);
-												});
-										});
-								});
-						}
+								callback(err);
+							}
+						);
 					},
-					function(err, results) {
-						if (err)
-							return callback(err);
-
-						cursor.commit(function(err) {
-							callback(err, results.book);
-						});
+					function(err) {
+						callback(err);
 					});
-				});
+				},
+				function(callback) {
+					cursor.query('INSERT INTO book_data(name, creator_credit_id, book_type_id, comment)' + 
+						' VALUES($1, $2, $3, $4) RETURNING book_data_id',
+						[ data.title, creator_credit_id, data.book_primary_type_id, data.comment ]).then(function(results) {
+						var book_data_id = results[0].book_data_id;
+
+						return cursor.query('INSERT INTO book_tree(book_data_id)' +
+							' VALUES($1) RETURNING book_tree_id', [ book_data_id ]);
+					}).then(function(results) {
+						var book_tree_id = results[0].book_tree_id;
+
+						return cursor.query('INSERT INTO book_revision(revision_id, book_id, book_tree_id)' +
+							' VALUES($1, $2, $3)', [ revision_id, book_id, book_tree_id ]);
+					}).done(
+						function() {
+							callback();
+						},
+						function(err) {
+							callback(err);
+						}
+					);
+				},
+			],
+			function(err) {
+				if (err)
+					return deferred.reject(err);
+
+				deferred.resolve();
 			});
-		});
+
+			return deferred.promise;
+		}).then(function() {
+			return cursor.commit();
+		}).done(
+			function() {
+				callback(undefined, book_id);
+			},
+			function(err) {
+				callback(err);
+			}
+		);
 	});
 };
 
