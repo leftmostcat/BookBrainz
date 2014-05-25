7e146a4599308319424f800164d0ea068f10ebfa
Seán de Búrca <leftmostcat@gmail.com>
2014-05-25 16:07:12 -0600

    Restore correct error handler functionality

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.use(function(req, res, next) {
 });
 
 if (app.get('env') === 'development') {
-	app.use(function(err, req, res) {
+	app.use(function(err, req, res, next) {
 		res.status(err.status || 500);
 		res.render('error', {
 			message: err.message,
@@ -70,7 +70,7 @@ if (app.get('env') === 'development') {
 	app.locals.pretty = true;
 }
 
-app.use(function(err, req, res) {
+app.use(function(err, req, res, next) {
 	res.status(err.status || 500);
 	res.render('error', {
 		message: err.message,
